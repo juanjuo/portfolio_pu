2e72b70799166555a53c3b7dfff89136369a04a2
Juan Diego Mora Rubio <juanjuo37@gmail.com>
2025-12-04 09:24:41 -0600

    fixed video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,39 @@
   import { base } from '$app/paths';
 </script> -->
 
-
-
-
 <body>
-  <header>
-    <h1>Juan Diego Mora</h1>
-    <h2>Composition Portfolio</h2>
-  </header>
-  <main>
-    <div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/i9ZQBw6s90A?si=JZ2RC1t7n7-efHI9" 
-      title="YouTube video player" frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-      </iframe>
-      <p>Title and some more info.</p>
-      <a href="https://drive.google.com/file/d/1Va2jMdLojAUctrXoQ_l2I5Z4Jho3K0oX/view?usp=sharing">Score Here</a>
-    </div>
+	<header>
+		<h1>Juan Diego Mora</h1>
+		<h2>Composition Portfolio</h2>
+	</header>
+	<main>
+		<div>
+			<iframe
+				width="560"
+				height="315"
+				src="https://www.youtube.com/embed/i9ZQBw6s90A?si=JZ2RC1t7n7-efHI9"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen>
+			</iframe>
+			<p>Title and some more info.</p>
+			<a href="https://drive.google.com/file/d/1Va2jMdLojAUctrXoQ_l2I5Z4Jho3K0oX/view?usp=sharing"
+				>Score Here</a>
+		</div>
 
-
-  </main>
-  <footer>
-    
-  </footer>
+		<div>
+			<p>this is another video inside a different div</p>
+		</div>
+	</main>
+	<footer>
+		<p>this is the footer</p>
+	</footer>
 </body>
 
 <!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 -->
+
+<!-- TO RUN USE = npm run deploy-->
